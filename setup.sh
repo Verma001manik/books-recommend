4ec94d246a512954217ba110c7831dd7d4b2692c
Manik verma <verma001manik@gmail.com>
2025-08-07 17:37:56 +0530

    man fuck this

--- setup.sh
+++ setup.sh
@@ -13,8 +13,8 @@ pip install git+https://github.com/Verma001manik/recommendme.git
 
 echo "Downloading FAISS index and metadata..."
 mkdir -p embeddings
-wget -O data/hf_cleaned_faiss_index.bin https://huggingface.co/datasets/verma001mani/hf_books_embed/resolve/main/hf_cleaned_faiss_index.bin
-wget -O data/hf_cleaned.json https://huggingface.co/datasets/verma001mani/hf_books_embed/resolve/main/hf_cleaned.json
+wget -O embeddings/hf_cleaned_faiss_index.bin https://huggingface.co/datasets/verma001mani/hf_books_embed/resolve/main/hf_cleaned_faiss_index.bin
+wget -O embeddings/hf_cleaned.json https://huggingface.co/datasets/verma001mani/hf_books_embed/resolve/main/hf_cleaned.json
 
 echo "Starting Flask app with Gunicorn..."
 gunicorn server:app --bind 0.0.0.0:8000
